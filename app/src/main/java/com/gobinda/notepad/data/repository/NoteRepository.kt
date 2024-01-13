2e4ef31286352806c6a8b018e7c81621df1383e4
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-01-13 17:16:31 +0600

    Title - Completing data layer implementation Details - Data layer includes database implementation + repository implementation

---
+++ app/src/main/java/com/gobinda/notepad/data/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.gobinda.notepad.data.repository
+
+import com.gobinda.notepad.data.model.NoteModel
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepository {
+
+    fun getAllNotes(): Flow<List<NoteModel>>
+
+    fun getSingleNote(noteId: Long): Flow<NoteModel?>
+
+    suspend fun addOrUpdateNote(note: NoteModel): Long
+
+    suspend fun deleteNote(noteId: Long): Int
+}
